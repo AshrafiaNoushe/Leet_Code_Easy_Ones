d46086eb64c8835ebee995e857b1ad7208910c4b
Ashrafia Noushe <ashrafianoushe779@gmail.com>
2025-05-19 22:27:52 +0600

    med

---
+++ Longest_Increasing_subsequence.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e5+10;
+int dp[N];
+vector<int>a(N);
+int subseq_inc(int i,vector<int>&a){
+    //if(i==0)return 0;
+    if(dp[i]!=-1){
+        return dp[i];
+    }
+    int ans = 1;
+    for(int j=0;j<i;++j){
+        if(a[i]>a[j]){
+            ans = max(ans,subseq_inc(j,a)+1);
+        }
+    }
+    return dp[i]=ans;
+
+}
+int main()
+{
+    memset(dp,-1,sizeof(dp));
+    int n;
+    cin>>n;
+    for(int i=0;i<n;++i){
+        cin>>a[i];
+    }
+    int ans = 0;
+    for(int i=0;i<n;++i){
+        ans = max (ans,subseq_inc(i,a));
+    }
+    cout<<ans<<endl;
+}
